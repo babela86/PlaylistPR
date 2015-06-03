599f51b293d2adfa19879bd9d3778e688e6bdd5d
pedrosrodrigues <pedrorodrigues.idl@gmail.com>
2015-06-03 11:29:23 +0100

    corrigir o commit anterior, hj a tarde deve haver mais...

--- firstear/theparent/aservice-services/src/main/java/dei/uc/pt/ar/Populate.java
+++ firstear/theparent/aservice-services/src/main/java/dei/uc/pt/ar/Populate.java
@@ -40,29 +40,29 @@ public class Populate implements Serializable {
 						UserRegister.encriptaPass("123"),
 						ft.parse("1986-04-30")) };
 
-		Playlist[] plays = { new Playlist("rafa's playlist", users[1].getIdUtilizador()),
-				new Playlist("p's playlist", users[0].getIdUtilizador()) };
+		Playlist[] plays = { new Playlist("rafa's playlist", users[1]),
+				new Playlist("p's playlist", users[0]) };
 
 		Musica[] musics = {
 				new Musica("Lean on", "Major Lazer & DJ Snake",
 						"Peace is the Mission", "2015",
-						"https://www.youtube.com/watch?v=YqeW9_5kURI", users[0].getIdUtilizador()),
+						"https://www.youtube.com/watch?v=YqeW9_5kURI", users[0]),
 				new Musica("O corpo é que paga", "António Variações",
-						"O melhor de António Variações", "1997", "qqcoisa.com", users[0].getIdUtilizador()),
+						"O melhor de António Variações", "1997", "qqcoisa.com", users[0]),
 				new Musica("Era só Jajão", "Master Jake", "nao sei", "2014",
-						"https://www.youtube.com/watch?v=vMgp904a3nc", users[0].getIdUtilizador()),
+						"https://www.youtube.com/watch?v=vMgp904a3nc", users[0]),
 				new Musica("Não me Toca", "Anselmo Ralph", "A dor de Cupido",
 						"2011",
-						"https://www.youtube.com/watch?v=whH2oDg28-k&list=RDwhH2oDg28-k", users[1].getIdUtilizador()),
+						"https://www.youtube.com/watch?v=whH2oDg28-k&list=RDwhH2oDg28-k", users[1]),
 				new Musica("YMCA", "Village People", "Cruisn", "1978",
-						"https://www.youtube.com/watch?v=CS9OO0S5w2k", users[1].getIdUtilizador()),
+						"https://www.youtube.com/watch?v=CS9OO0S5w2k", users[1]),
 				new Musica("I wanna Dance with Somebody (Who Loves Me)",
 						"Whitney Houston", "Whitney", "1987",
-						"https://www.youtube.com/watch?v=eH3giaIzONA", users[1].getIdUtilizador()),
+						"https://www.youtube.com/watch?v=eH3giaIzONA", users[1]),
 				new Musica("Show das Poderosas", "Anitta", "Nao sei", "2013",
-						"https://www.youtube.com/watch?v=FGViL3CYRwg", users[1].getIdUtilizador()),
+						"https://www.youtube.com/watch?v=FGViL3CYRwg", users[1]),
 				new Musica("No woman no cry", "Bob Marley", "Natty Dread",
-						"1974", "https://www.youtube.com/watch?v=jGqrvn3q1oo", users[0].getIdUtilizador()) };
+						"1974", "https://www.youtube.com/watch?v=jGqrvn3q1oo", users[0]) };
 
 		plays[0].addMusica(musics[1]); 
 		plays[0].addMusica(musics[0]);
