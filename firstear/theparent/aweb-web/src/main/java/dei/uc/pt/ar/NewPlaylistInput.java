ebc8e9908ae32453153b24d72bb3fde8e710302d
babela86 <lourencorafaela@gmail.com>
2015-06-04 16:38:01 +0100

    formataçao

--- firstear/theparent/aweb-web/src/main/java/dei/uc/pt/ar/NewPlaylistInput.java
+++ firstear/theparent/aweb-web/src/main/java/dei/uc/pt/ar/NewPlaylistInput.java
@@ -10,50 +10,46 @@ import javax.inject.Named;
 
 @SessionScoped
 @Named
-public class NewPlaylistInput implements Serializable{
-
+public class NewPlaylistInput implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private PlaylistDAO pd;
 	@Inject
 	private UserInput ui;
-	
+
 	private String name;
-	
-	public NewPlaylistInput(){
-		
+
+	public NewPlaylistInput() {
+
 	}
-	
-	
+
 	public String newPlaylist() {
 		Playlist p = new Playlist(this.name, ui.getActiveUser());
 		boolean added = pd.newPlaylist(p);
-		if (added==true){
-			FacesMessage msg = new FacesMessage("Playlist added to DB!", "INFO MSG");
+		if (added == true) {
+			FacesMessage msg = new FacesMessage("Playlist added to DB!",
+					"INFO MSG");
 			msg.setSeverity(FacesMessage.SEVERITY_INFO);
 			if (FacesContext.getCurrentInstance() != null)
 				FacesContext.getCurrentInstance().addMessage(null, msg);
 		} else {
-			FacesMessage msg = new FacesMessage("Playlist already exists!", "ERROR MSG");
+			FacesMessage msg = new FacesMessage("Playlist already exists!",
+					"ERROR MSG");
 			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
 			if (FacesContext.getCurrentInstance() != null)
 				FacesContext.getCurrentInstance().addMessage(null, msg);
 		}
-		return "addPlaylists";
+		return "addPlaylist";
 	}
-
 
 	public String getName() {
 		return name;
 	}
 
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
 
 }
