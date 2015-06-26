b817998f6c863d431abe82d606d45a2f51fcafb1
pedrosrodrigues <pedrorodrigues.idl@gmail.com>
2015-06-26 12:01:41 +0100

    committtttttttttt
    ta quase o ws e o client.

--- firstear/theparent/thews-ws/src/main/java/dei/uc/pt/ar/paj/SimpleMusicService.java
+++ firstear/theparent/thews-ws/src/main/java/dei/uc/pt/ar/paj/SimpleMusicService.java
@@ -10,6 +10,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import dei.uc.pt.ar.MusicDAO;
 import dei.uc.pt.ar.Musica;
@@ -57,6 +58,41 @@ public class SimpleMusicService {
 		return md.getMusic(id);
 	}
 	
+	//Remover musica concreta de um user (passa para o gestor)
+	@GET
+	@Path("/delete/{musicId}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Response removeUserById(@PathParam("musicId") int id){		
+		boolean removed = md.deleteMusicFromDb(id);
+		if (removed)
+			return Response.ok().build();
+		else
+			return Response.notModified().build();
+	}
+	
+	//Adicionar música a uma playlist
+	@GET
+	@Path("/add/{musicId}/toplaylist/{playId}")
+	@Produces({MediaType.APPLICATION_XML})
+	public Response addToPlaylist(@PathParam("musicId") int idm, @PathParam("playId") int idp){		
+		boolean added = md.addTo(idp, idm);
+		if (added)
+			return Response.ok().build();
+		else
+			return Response.notModified().build();
+	}
+	
+	//Remover música a uma playlist
+	@GET
+	@Path("/remove/{musicId}/fromplaylist/{playId}")
+	@Produces({MediaType.APPLICATION_XML})
+	public Response removeFromPlaylist(@PathParam("musicId") int idm, @PathParam("playId") int idp){		
+		boolean added = md.deleteMusic(idp, idm);
+		if (added)
+			return Response.ok().build();
+		else
+			return Response.notModified().build();
+	}
 	
 	
 	
