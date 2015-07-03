e317d9de0a416d7617b6e962a1baaad44706770b
ciclapo <ciclapo@hotmail.com>
2015-07-03 14:41:54 +0100

    alteração background e css

--- firstear/theparent/aservice-services/src/main/java/dei/uc/pt/ar/Populate.java
+++ firstear/theparent/aservice-services/src/main/java/dei/uc/pt/ar/Populate.java
@@ -31,7 +31,7 @@ public class Populate implements Serializable {
 
 	public void populando() throws ParseException, NoSuchAlgorithmException,
 			UnsupportedEncodingException {
-	
+
 		Utilizador[] users = {
 				new Utilizador("gestor@playlist.com", "gestor",
 						UserRegister.encriptaPass("gestor"),
@@ -39,7 +39,7 @@ public class Populate implements Serializable {
 				new Utilizador("pedro@gmail.com", "pedro",
 						UserRegister.encriptaPass("123"),
 						ft.parse("1983-01-01")),
-				new Utilizador("caty@gmail.com", "rafa",
+				new Utilizador("caty@gmail.com", "caty",
 						UserRegister.encriptaPass("123"),
 						ft.parse("1986-04-30")) };
 
@@ -51,12 +51,17 @@ public class Populate implements Serializable {
 						"Peace is the Mission", "2015",
 						"https://www.youtube.com/watch?v=YqeW9_5kURI", users[1]),
 				new Musica("O corpo é que paga", "António Variações",
-						"O melhor de António Variações", "1997", "qqcoisa.com", users[1]),
+						"O melhor de António Variações", "1997", "qqcoisa.com",
+						users[1]),
 				new Musica("Era só Jajão", "Master Jake", "nao sei", "2014",
 						"https://www.youtube.com/watch?v=vMgp904a3nc", users[1]),
-				new Musica("Não me Toca", "Anselmo Ralph", "A dor de Cupido",
+				new Musica(
+						"Não me Toca",
+						"Anselmo Ralph",
+						"A dor de Cupido",
 						"2011",
-						"https://www.youtube.com/watch?v=whH2oDg28-k&list=RDwhH2oDg28-k", users[2]),
+						"https://www.youtube.com/watch?v=whH2oDg28-k&list=RDwhH2oDg28-k",
+						users[2]),
 				new Musica("YMCA", "Village People", "Cruisn", "1978",
 						"https://www.youtube.com/watch?v=CS9OO0S5w2k", users[2]),
 				new Musica("I wanna Dance with Somebody (Who Loves Me)",
@@ -65,14 +70,15 @@ public class Populate implements Serializable {
 				new Musica("Show das Poderosas", "Anitta", "Nao sei", "2013",
 						"https://www.youtube.com/watch?v=FGViL3CYRwg", users[2]),
 				new Musica("No woman no cry", "Bob Marley", "Natty Dread",
-						"1974", "https://www.youtube.com/watch?v=jGqrvn3q1oo", users[1]) };
+						"1974", "https://www.youtube.com/watch?v=jGqrvn3q1oo",
+						users[1]) };
 
-		plays[0].addMusica(musics[1]); 
+		plays[0].addMusica(musics[1]);
 		plays[0].addMusica(musics[0]);
 		plays[1].addMusica(musics[2]);
-		plays[1].addMusica(musics[0]); 
+		plays[1].addMusica(musics[0]);
 		plays[0].addMusica(musics[4]);
-		plays[0].addMusica(musics[5]); 
+		plays[0].addMusica(musics[5]);
 		plays[1].addMusica(musics[6]);
 		plays[0].addMusica(musics[7]);
 
